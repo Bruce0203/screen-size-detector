f0939a763d7d0674a9ee642bf218cd496875d016
Bruce0203 <BruceFreedy@gmail.com>
2023-08-17 11:15:31 +0900

    init

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,35 +1,68 @@
-import korlibs.time.*
 import korlibs.korge.*
 import korlibs.korge.scene.*
-import korlibs.korge.tween.*
 import korlibs.korge.view.*
 import korlibs.image.color.*
-import korlibs.image.format.*
-import korlibs.io.file.std.*
+import korlibs.image.text.*
+import korlibs.korge.view.align.*
 import korlibs.math.geom.*
-import korlibs.math.interpolation.*
 
-suspend fun main() = Korge(windowSize = Size(512, 512), backgroundColor = Colors["#2b2b2b"]) {
-	val sceneContainer = sceneContainer()
+val color1 = Colors["#2b2b2b"]
+val color2 = Colors.WHITESMOKE
 
-	sceneContainer.changeTo({ MyScene() })
+suspend fun main() {
+    Korge(
+        windowSize = Size(512, 512), backgroundColor = color1,
+        displayMode = KorgeDisplayMode(
+            scaleMode = ScaleMode.SHOW_ALL,
+            scaleAnchor = Anchor.TOP_LEFT,
+            clipBorders = false
+        )
+    ) {
+        val sceneContainer = sceneContainer()
+        sceneContainer.changeTo({ MyScene() })
+    }
 }
 
 class MyScene : Scene() {
 	override suspend fun SContainer.sceneMain() {
-		val minDegrees = (-16).degrees
-		val maxDegrees = (+16).degrees
+        fixedSizeContainer(size) {
+            val text = text(
+                "", textSize = 25f, alignment = TextAlignment.MIDDLE_CENTER, color = color2
+            ).zIndex(5).centerOnStage()
+            var padding = 20f
+            val container = fixedSizeContainer(size).centerOnStage()
+            val top = container
+                .solidRect(Size(width-padding, padding), color2)
+                .positionY(padding)
+            val bottom = container
+                .solidRect(Size(width-padding, padding), color2)
+                .positionY(height-padding*2)
+            val left = container
+                .solidRect(Size(padding, height-padding), color2)
+                .positionX(padding)
+            val right = container
+                .solidRect(Size(padding, height-padding), color2)
+                .positionX(width-padding*2)
 
-		val image = image(resourcesVfs["korge.png"].readBitmap()) {
-			rotation = maxDegrees
-			anchor(.5, .5)
-			scale(0.8)
-			position(256, 256)
-		}
+            onStageResized { width, height ->
+//                container.size(width, height)
+                top.width = width.toFloat() - padding*2
+                top.positionY(padding)
+                top.positionX(padding)
+                bottom.width = width.toFloat() - padding*2
+                bottom.positionY(height.toFloat() - padding*2)
+                bottom.positionX(padding)
+                left.height = height.toFloat() - padding*2
+                left.positionX(padding)
+                left.positionY(padding)
+                right.height = height.toFloat() - padding*2
+                right.positionX(width.toFloat() - padding*2)
+                right.positionY(padding)
 
-		while (true) {
-			image.tween(image::rotation[minDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-			image.tween(image::rotation[maxDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
-		}
-	}
+                text.text = "${width}x${height}"
+                text.centerOnStage()
+            }
+        }
+
+    }
 }
